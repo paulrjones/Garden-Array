68c94a4d9ae94102ed6cf8656af2f1d855affecd
Carlo Nazareno <csnazareno94@gmail.com>
2020-03-27 10:41:11 -0700

    commit to pull from master

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,35 +1,72 @@
 import React, { useState } from 'react'
-import { Button } from '@material-ui/core'
+import {
+    TextField,
+    InputAdornment,
+    IconButton,
+    Icon,
+    Container
+} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 import { Redirect } from 'react-router-dom'
-import axios from 'axios'
+// import axios from 'axios'
 
 const Home = () => {
 
-    // const [sessionState, setSessionState] = useState({
-    //     redirect: false
-    // })
+    const [sessionState, setSessionState] = useState({
+        redirect: false,
+        searchPlant: ''
+    })
 
-    const handleLogOut = () => {
-        axios.get('/api/users/logout')
-            .then(user => {
-                // setSessionState({ ...sessionState, redirect: true })
-            })
-            .catch(e => console.error(e))
+    const handleInputChange = ({ target }) => {
+        setSessionState({ ...sessionState, [target.name]: target.value })
     }
+
+    const handleInputSubmit = event => {
+        event.preventDefault()
+        console.log(sessionState.searchPlant)
+        
+        setSessionState({ ...sessionState, searchPlant: '' })
+    }
+
+    // const handleLogOut = () => {
+    //     axios.get('/api/users/logout')
+    //         .then(user => {
+    //             console.log(user)
+    //             setSessionState({ ...sessionState, redirect: true })
+    //         })
+    //         .catch(e => console.error(e))
+    // }
 
     return (
         <>
-            {/* {sessionState.redirect ? <Redirect to={{ pathname: '/signin' }} /> :
-                ( */}
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={handleLogOut}
-                >
-                    Logout
-                </Button>
-                {/* )
-             } */}
+            {sessionState.redirect ? <Redirect to={{ pathname: '/signin' }} /> :
+                (
+                    <Container>
+                        <form>
+                            <TextField
+                                label="With normal TextField"
+                                id="searchPlant"
+                                name="searchPlant"
+                                value={sessionState.searchPlant}
+                                onChange={handleInputChange}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment>
+                                            <IconButton
+                                                onClick={handleInputSubmit}
+                                            >
+                                                <Icon>
+                                                    <SearchIcon />
+                                                </Icon>
+                                            </IconButton>
+                                        </InputAdornment>
+                                    )
+                                }}
+                            />
+                        </form>
+                    </Container>
+                )
+            }
         </>
     )
 }
